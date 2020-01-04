420c5bbdad7bb7c6decb1613c3ecf5e940a7a5bd
ChangShuYu <13955220740@139.com>
2020-01-04 21:04:19 +0800

    edit Model_aX_Train.py and save Model4ax

--- paddle/Model_aX_Train.py
+++ paddle/Model_aX_Train.py
@@ -32,22 +32,24 @@ import numpy
 当x=4时，y=8
 '''
 train_data=numpy.array([[1.0],[2.0],[3.0],[4.0]]).astype('float32') # x的取值
-y_true = numpy.array([[2.0],[4.0],[6.0],[8.0]]).astype('float32') # y的取值
 
-#定义网络
+#定义仅有正向传播的预测网络
 #输入层
 x = fluid.data(name="x",shape=[-1,1],dtype='float32') #原文为 fluid.layers.data
-y = fluid.layers.data(name="y",shape=[1],dtype='float32') #原文为 fluid.layers.data
 #全连接层
 y_predict = fluid.layers.fc(input=x,size=1,act=None)
 
+#增加反向传播网络
+#定义标签
+y_true = numpy.array([[2.0],[4.0],[6.0],[8.0]]).astype('float32') # y的取值
+#输入层
+y = fluid.layers.data(name="y",shape=[1],dtype='float32') #原文为 fluid.layers.data
 #定义损失函数,评估预测结果的好坏
 cost = fluid.layers.square_error_cost(input=y_predict,label=y) # 采用方差函数
 avg_cost = fluid.layers.mean(cost) # 采用方差函数
-
 #定义优化方法
-sgd_optimizer = fluid.optimizer.SGD(learning_rate=0.01)
-sgd_optimizer.minimize(avg_cost)
+sgd_optimizer = fluid.optimizer.SGD(learning_rate=0.01) # SGD优化器
+sgd_optimizer.minimize(avg_cost) # 根据反向计算所得的梯度，更新权重，使得avg_cost最小
 
 #参数初始化
 cpu = fluid.core.CPUPlace()
@@ -61,3 +63,12 @@ for i in range(100):
         fetch_list=[y_predict.name,avg_cost.name])
 #观察结果
 print(outs)
+
+# 存储训练后的模型
+params_dirname = 'Model4ax'
+fluid.io.save_inference_model(
+    params_dirname, # dirname: 保存预测模型的文件夹
+    ['x'], # feeded_var_names: 预测时所需提供数据的所有输入变量的名称
+    [y_predict], # target_vars：模型的输出变量
+    exe) # executor：模型的执行器
+
